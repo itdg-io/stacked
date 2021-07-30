65509c5b7dd9ac82f14f49a48b87663d8f9d2307
KAIMonmoy <aftarulislam@gmail.com>
2021-07-30 09:08:34 +0600

    :sparkles: Finalized Registration

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,54 @@
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+$('#reg-email').on('input propertychange', function (e) {
+    console.log('Changed');
+    $('#reg-err').text('');
+});
+
 $('#reg-btn').click(function (e) {
     e.preventDefault();
-    email = $('#reg-email').val();
-    time = new Date();
-    if (email) {
-        data = {
-            email,
-            created: time
-        }
+    const email = $('#reg-email').val();
+    const time = new Date();
+    const type = $("#c-1").hasClass('active') ? "holder" : $("#c-2").hasClass('active') ? "issuer" : "verifier";
+    data = {
+        type,
+        email,
+        created: time
+    }
+    if (!validateEmail(email)) {
+        $('#reg-err').text('Please enter a valid e-mail!');
+        return;
+    }
+    try {
         const collection = firebase.firestore().collection('registrations');
-        $('#reg-email').val('');
         const result = collection.add(data);
-        console.log(result);
-    } else {
-        console.log('Invalid Data!');
+        $('#reg-email').val('');
+        $('#reg-btn').addClass('bg-success');
+        $('#reg-btn').attr('value', 'Success!');
+        $('#reg-btn').attr('disabled', 'true');
+        $('#reg-err').addClass('text-success');
+        $('#reg-err').text('Registration completed successfully!');
+        setTimeout(() => {
+            $('#reg-btn').removeClass('bg-success');
+            $('#reg-btn').attr('value', 'Sign Up');
+            $('#reg-btn').removeAttr('disabled');
+            $('#reg-err').removeClass('text-success');
+            $('#reg-err').text('');
+        }, 2000);
+    } catch (ex) {
+        $('#reg-btn').addClass('bg-danger');
+        $('#reg-btn').attr('value', 'Failed!');
+        $('#reg-btn').attr('disabled', 'true');
+        $('#reg-err').text('Failed to completed registration!');
+        setTimeout(() => {
+            $('#reg-btn').removeClass('bg-danger');
+            $('#reg-btn').attr('value', 'Sign Up');
+            $('#reg-btn').removeAttr('disabled');
+            $('#reg-err').text('');
+        }, 2000);
     }
 
 })
