b31bb382a3319ee2cfeb4fa2952cfdfb6709637c
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2021-07-30 17:20:58 +0600

    :iphone: made two sections responsive
    "How does it work?" and "Imagine" sections are now responsive.

--- js/header.js
+++ js/header.js
@@ -49,8 +49,8 @@ $('#button-holder').click(function () {
           $('.sign-header-main').replaceWith("<h1 class='sign-header-main'>The Holder</h1>");
      });
      $('.how-para-main').fadeIn(5, function () {
-          $('.how-para').replaceWith('<p class="how-para">Everyday-people who hold academic credentials. The holder will store each digital credential in a digital wallet that is privacy preserving and completely controlled by the owner.</p>')
-          $('.how-para-1').replaceWith('<p class="how-para-1">Example of credentials that can be stored :</p>')
+          $('.how-para').replaceWith('<p class="how-para px-5">Everyday-people who hold academic credentials. The holder will store each digital credential in a digital wallet that is privacy preserving and completely controlled by the owner.</p>')
+          $('.how-para-1').replaceWith('<p class="how-para-1 px-5">Example of credentials that can be stored :</p>')
      });
      $('.how-list').fadeIn(5, function () {
           $('.how-list-1').replaceWith('<dd class="how-list-1">-  &nbsp;&nbsp;Graduation Certificate</dd>')
@@ -93,8 +93,8 @@ $('#button-verifier').click(function () {
 
      
      $('.how-para-main').fadeIn(5, function () {
-          $('.how-para').replaceWith('<p class="how-para">The verifier is an entity or organisation that wishes to verify claims or credentials of a certain individual so that they can provide a service. They can make requests to see specific information in a credential.</p>')
-          $('.how-para-1').replaceWith('<p class="how-para-1">Examples of Verifiers:</p>')
+          $('.how-para').replaceWith('<p class="how-para px-5">The verifier is an entity or organisation that wishes to verify claims or credentials of a certain individual so that they can provide a service. They can make requests to see specific information in a credential.</p>')
+          $('.how-para-1').replaceWith('<p class="how-para-1 px-5">Examples of Verifiers:</p>')
      });
      $('.how-list').fadeIn(5, function () {
           $('.how-list-1').replaceWith('<dd class="how-list-1">-  &nbsp;&nbsp;Universities</dd>')
@@ -130,8 +130,8 @@ $('#button-issuer').click(function () {
           $('.sign-header-main').replaceWith("<h1 class='sign-header-main'>The Issuer</h1>");
      });
      $('.how-para-main').fadeIn(5, function () {
-          $('.how-para').replaceWith('<p class="how-para">The issuer is an entity or organisation that issues credentials or other form of claims to individuals.</p>')
-          $('.how-para-1').replaceWith('<p class="how-para-1">Example of issuers:</p>')
+          $('.how-para').replaceWith('<p class="how-para px-5">The issuer is an entity or organisation that issues credentials or other form of claims to individuals.</p>')
+          $('.how-para-1').replaceWith('<p class="how-para-1 px-5">Example of issuers:</p>')
      });
      $('.how-list').fadeIn(5, function () {
           $('.how-list-1').replaceWith('<dd class="how-list-1">-  &nbsp;&nbsp;High Schools</dd>')
