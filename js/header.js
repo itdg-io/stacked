5107fdfb14dc80c2baab44ef5d3f04fce5aeabe7
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2021-07-30 17:03:29 +0600

    :sparkles: changed Hermione to Lily in all instances. Updated some text according to figma.

--- js/header.js
+++ js/header.js
@@ -49,8 +49,8 @@ $('#button-holder').click(function () {
           $('.sign-header-main').replaceWith("<h1 class='sign-header-main'>The Holder</h1>");
      });
      $('.how-para-main').fadeIn(5, function () {
-          $('.how-para').replaceWith('<p class="how-para">Everyday people who hold academic credentials. The holder will store each digital credential in a digital wallet that is privacy preserving and completely controlled by the owner.</p>')
-          $('.how-para-1').replaceWith('<p class="how-para-1">Example of holders:</p>')
+          $('.how-para').replaceWith('<p class="how-para">Everyday-people who hold academic credentials. The holder will store each digital credential in a digital wallet that is privacy preserving and completely controlled by the owner.</p>')
+          $('.how-para-1').replaceWith('<p class="how-para-1">Example of credentials that can be stored :</p>')
      });
      $('.how-list').fadeIn(5, function () {
           $('.how-list-1').replaceWith('<dd class="how-list-1">-  &nbsp;&nbsp;Graduation Certificate</dd>')
@@ -88,8 +88,12 @@ $('#button-verifier').click(function () {
           $('.sign-all').replaceWith('<div class="sign-verifier sign-all">V</div>')
           $('.sign-header-main').replaceWith("<h1 class='sign-header-main'>The Verifier</h1>");
      });
+
+     
+
+     
      $('.how-para-main').fadeIn(5, function () {
-          $('.how-para').replaceWith('<p class="how-para">The verifier is an entity or organisation that wishes to verify claims or credentials of a certain individual. They can make requests to see specific information in a credential.</p>')
+          $('.how-para').replaceWith('<p class="how-para">The verifier is an entity or organisation that wishes to verify claims or credentials of a certain individual so that they can provide a service. They can make requests to see specific information in a credential.</p>')
           $('.how-para-1').replaceWith('<p class="how-para-1">Examples of Verifiers:</p>')
      });
      $('.how-list').fadeIn(5, function () {
